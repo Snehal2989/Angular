730f332f2a76f45db7d340ce2908bea0df088c27
Snehal2989 <punsesnehal@gmail.com>
2021-10-26 23:12:49 +0530

    angular form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Routingproject';
+
+  _couList!: Course[];
+
+  ngOnInt()
+  {
+    this.getCourse();
+  }
+
+  getCourse()
+{
+  this._couList=[
+    
+    {id:1,cname:"java",isselected:false},
+    {id:2,cname:"Advance-java",isselected:false},
+    {id:3,cname:"PHP",isselected:false},
+    {id:4,cname:"Salesforce",isselected:false},
+    {id:5,cname:"Python",isselected:false}
+    
+  ]
+
+  
 }
+
+onselect()
+{
+  console.log(this._couList)
+}
+
+}
+   
+
+  class Course {
+    id!:number;
+    cname!:string;
+    isselected!:boolean
+
+  }
+
+
